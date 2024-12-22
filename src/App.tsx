9c6b3e5f87d5dd7d4c9afea1ca859504171376f4
ceoDemitri <demichole@gmail.com>
2024-12-22 08:22:16 -0600

    base layout and funcions laid out

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import './App.css'
+import { Badge } from "@/components/ui/badge"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
 
 function App() {
   // script things here
@@ -8,6 +12,11 @@ function App() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Explore the Big Outdoors
       </h1>
+      <Badge variant="outline">Twinkle Twinkle little staer</Badge>
+      <div className="flex w-full mx-auto max-w-md">
+        <Input type="text" placeholder="topic" />
+        <Button type="submit">Search</Button>
+      </div>
     </>
   )
 }

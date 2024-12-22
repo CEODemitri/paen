27b82c25dec8ccd07f4fdda14fc63072c7299e98
ceoDemitri <demichole@gmail.com>
2024-12-22 08:09:25 -0600

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  // script things here
+
+  return (
+    <>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Explore the Big Outdoors
+      </h1>
+    </>
+  )
+}
+
+export default App
